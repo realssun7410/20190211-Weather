977908c50a4a597ea80e668aa45a13985db1e5d5
Hyejin Park <hyejin.park@one-line.com>
2019-02-11 17:44:17 +0900

    weekly까지 작업중

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,17 @@ var apiURL = "https://api.openweathermap.org/data/2.5/";
 var appid = "a1721956201bd612760834ddeb2d039e";
 var units = "metric";
 var files = ["weather", "forecast"];
+var option = {
+    appid: appid,
+    units: units
+};
 
 //modal init
+cityInit();
+
+function cityInit() {
+    $("#modal").show();
+}
 $.ajax({
     type: "get",
     url: "../json/city.json",
@@ -32,7 +41,7 @@ $(".nav").click(function () {
         "border-right": "5px solid #fff",
         "border-bottom": "5px solid #666",
         "border-left": "5px solid #fff",
-        "z-index":10
+        "z-index": 10
     });
     $(this).css({
         "background-color": "#666",
@@ -41,10 +50,67 @@ $(".nav").click(function () {
         "border-right": "5px solid #666",
         "border-bottom": "5px solid #fff",
         "border-left": "5px solid #666",
-        "z-index":10
+        "z-index": 10
     });
     $(".cont").hide(0);
     $(".cont").eq(n).show(0);
 
 });
 $(".nav").eq(0).trigger("click");
+
+$("#city").change(function () {
+    option.id = $(this).val();
+    var sendData = {
+        type: "get",
+        dataType: "json",
+        data: option,
+    }
+    sendData.url = apiURL + files[0];
+    sendData.success = dailyInit;
+    $.ajax(sendData);
+    sendData.url = apiURL + files[1];
+    sendData.success = weeklyInit;
+    $.ajax(sendData);
+});
+
+function dailyInit(data) {
+    console.log(data);
+    $("#modal").hide();
+    var $daily = $("#daily");
+    var src = "../img/icon/" + data.weather[0].icon + ".png";
+    var temp = data.main.temp + " ℃";
+    var temp2 = data.main.temp_max + " ℃ /" + data.main.temp_min + " ℃ /";
+    var html = '';
+    html += '<ul>';
+    html += '<li class="icon"><img src="' + src + '" class="img"></li>';
+    html += '<li class="city_name">' + $("#city > option:selected").text() + '</li>';
+    html += '<li class="w3-center"><button class="w3-button w3-indigo" onclick="cityInit();">도시선택</button></li>';
+    html += '<li class="temp">현재평균온도: <b>' + temp + '</b></li>';
+    html += '<li class="temp2">최고/최저온도: <b>' + temp2 + '</b></li>';
+    html += '</ul>';
+    $daily.html(html);
+};
+
+function weeklyInit(data) {
+    console.log(data);
+    $("#modal").hide();
+    var src, temp, temp2, date;
+    var $weekly = $("#weekly");
+    var html = '<div>';
+    for (var i in data.list) {
+        src = "../img/icon/" +data.weather[0].icon + ".png";
+        date =data.list[i].dt_text;
+        temp = data.list[i].main.temp+ " ℃";
+        temp2 = data.list[i].main.temp_max+ " ℃ /" + data.list[i].main.temp_max + " ℃ /";
+        html += '<ul>';
+        html += '<li><img= src="'+src+'" class ="img"></li>';
+        html += '<li>';
+        html += '<div>예보날짜: '+date+'</div>';
+        html += '<div>현재온도: '+temp+'</div>';
+        html += '<div>최고/최저온도: '+temp2+'</div>';
+        html += '</li>';
+        html += '</ul>';
+    }
+    html += '</div>';
+    $(this).
+}
